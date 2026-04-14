977c52a1783edd7d407ab9357ab6b5c0135c8187
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-04-14 11:46:36 +0200

    part2

--- client/src/components/CarousselLarge/CarousselLarge.tsx
+++ client/src/components/CarousselLarge/CarousselLarge.tsx
@@ -21,19 +21,17 @@ function CarousselLarge({
       <h2 className="caroussel-titre">{titre}</h2>
 
       <div className="caroussel-center">
-        {items.length > 0 ? (
-          items.map((item) => (
-            <div key={item.id} className="caroussel-item">
-           
-            </div>
-          ))
-        ) : (
-          Array.from({ length: 6}, (_, i) => `placeholder-${i}`).map((key) => (
-            <div key={key} className="caroussel-item">
-             <LittleCard />
-            </div>
-          ))
-        )}
+        {items.length > 0
+          ? items.map((item) => (
+              <div key={item.id} className="caroussel-item"></div>
+            ))
+          : Array.from({ length: 6 }, (_, i) => `placeholder-${i}`).map(
+              (key) => (
+                <div key={key} className="caroussel-item">
+                  <LittleCard />
+                </div>
+              ),
+            )}
       </div>
     </div>
   );
