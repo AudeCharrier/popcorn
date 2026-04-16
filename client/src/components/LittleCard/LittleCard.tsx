b4f0f07fc4e175a5f8342346682b99e7ae843211
DonDFlamingo <adrianpailhes31@gmail.com>
2026-04-16 10:18:28 +0200

    save little card state

--- client/src/components/LittleCard/LittleCard.tsx
+++ client/src/components/LittleCard/LittleCard.tsx
@@ -1,6 +1,8 @@
 import "./Littlecard.css";
+import { useNavigate } from "react-router";
 
 type CinemaProps = {
+  id: number;
   title: string;
   vote_average: number;
   release_date: string;
@@ -9,12 +11,20 @@ type CinemaProps = {
 };
 
 function LittleCard({
+  id,
   title,
   vote_average,
   release_date,
   overview,
   poster_path,
 }: CinemaProps) {
+  const navigate = useNavigate();
+  const handleSelectMovie = (id: number) => {
+    navigate("/films-series", {
+      state: { id },
+    });
+  };
+
   return (
     <div className="card">
       <div className="card__side card__side--front card__side--front-1">
@@ -31,7 +41,11 @@ function LittleCard({
       <div className="card__side card__side--back card__side--back-1">
         <div className="card__description">
           <p>{overview}</p>
-          <button className="card__side--back button" type="button">
+          <button
+            className="card__side--back button"
+            type="button"
+            onClick={() => handleSelectMovie(id)}
+          >
             Plus d'infos
           </button>
         </div>
