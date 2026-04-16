b60bf45cd4ca92d3061d30b188c1891c1075845e
bosilaugusta-hue <bosilaugusta@gmail.com>
2026-04-16 17:19:41 +0200

    Update Seances page

--- client/src/pages/Seances/Seances.tsx
+++ client/src/pages/Seances/Seances.tsx
@@ -1,9 +1,26 @@
 import "./Seances.css";
+import CarousselLarge from "../../components/CarousselLarge/CarousselLarge";
 
 function Seances() {
   return (
     <div className="Seances">
-      <h1>Seances</h1>
+      <div className="container">
+        <h1>ACTUELLEMENT EN SALLE</h1>
+
+        <div className="section-box">
+          <h2 className="h2-seances">SORTIES DE LA SEMAINE</h2>
+          <div className="carousel-wrapper-fix">
+            <CarousselLarge />
+          </div>
+        </div>
+
+        <div className="section-box">
+          <h2 className="h2-seances">TOUJOURS À L'AFFICHE</h2>
+          <div className="carousel-wrapper-fix">
+            <CarousselLarge />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
