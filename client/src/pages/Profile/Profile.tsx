81b5ff57ee8691eefc8f84bdda7bbf57b7f69915
AUDE CHARRIER <charrier.aude@gmail.com>
2026-04-28 12:38:44 +0200

    profile-menu : icons, titles, link-anchors and css

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
-import CarousselLarge from "../../components/CarousselLarge/CarousselLarge";
-import "./Profile.css";
 import BadgeWatcher from "../../assets/images/badge_watcher_amateur.png";
+/* import Deconnexion from "../../assets/images/deconnexion.png";
+import Favori from "../../assets/images/favori.png";
+import Paramètres from "../../assets/images/parametres.png"; */
 import Pencil from "../../assets/images/pencil.webp";
+import CarousselLarge from "../../components/CarousselLarge/CarousselLarge";
+/* import Profil from "../../assets/images/profil.png";
+import Watch from "../../assets/images/enregistrer.png"; */
+import "./Profile.css";
 
 type CarouselItem = {
   id: number;
@@ -15,13 +20,19 @@ type CarouselItem = {
 };
 
 function Profile() {
+  const width = {
+    width: "60vw",
+  };
+
+  const [activeLink, setActiveLink] = useState<string>("");
   const [favorites, setFavorites] = useState<CarouselItem[]>([]);
 
+  /*recuperer la liste favorites*/
   useEffect(() => {
     const stored = JSON.parse(localStorage.getItem("favorites") || "[]");
     setFavorites(stored);
   }, []);
-
+  /*supprimer un favori*/
   const removeFavorite = (id: number) => {
     const updated = favorites.filter((movie) => movie.id !== id);
     localStorage.setItem("favorites", JSON.stringify(updated));
@@ -29,34 +40,79 @@ function Profile() {
   };
 
   const [toWatch, setToWatch] = useState<CarouselItem[]>([]);
-
+  /*recuperer la liste watch*/
   useEffect(() => {
     const watchStored = JSON.parse(localStorage.getItem("towatch") || "[]");
     setToWatch(watchStored);
   }, []);
-
+  /*supprimer un watch*/
   const removeToWatch = (id: number) => {
     const watchUpdated = toWatch.filter((movie) => movie.id !== id);
     localStorage.setItem("towatch", JSON.stringify(watchUpdated));
     setToWatch(watchUpdated);
   };
 
-  const width = {
-    width: "60vw",
-  };
-
   return (
-    <>
-      <section className="profile-menu">
-        <p>Mon Profil</p>
-        <p>Mon Compte</p>
-        <p>Mes Favoris</p>
-        <p>Ma Watch-List</p>
-        <p>Paramètres</p>
-        <p>Déconnexion</p>
-      </section>
+    <section className="profile-general-wrapper">
+      <aside className="profile-menu">
+        <p className="profile-menu-titles profile-menu-my-profile">
+          Mon Profil
+        </p>
+        <ul className="profile-menu-list">
+          <li>
+            {/*biome-ignore lint/a11y/useValidAnchor : lien de navigation a pour une ancre*/}
+            <a
+              href="#compte"
+              className={`profile-menu-titles ${activeLink === "compte" ? "profile-menu-selected" : ""}`}
+              id="mon-compte"
+              onClick={() => setActiveLink("compte")}
+            >
+              Mon Compte
+            </a>
+          </li>
+          <li>
+            {/*biome-ignore lint/a11y/useValidAnchor : lien de navigation a pour une ancre*/}
+            <a
+              href="#favoris"
+              className={`profile-menu-titles ${activeLink === "favoris" ? "profile-menu-selected" : ""}`}
+              id="mes-favoris"
+              onClick={() => setActiveLink("favoris")}
+            >
+              Mes Favoris
+            </a>
+          </li>
+          <li>
+            {/*biome-ignore lint/a11y/useValidAnchor : lien de navigation a pour une ancre*/}
+            <a
+              href="#watch-list"
+              className={`profile-menu-titles ${activeLink === "watch-list" ? "profile-menu-selected" : ""}`}
+              id="ma-watch-list"
+              onClick={() => setActiveLink("watch-list")}
+            >
+              Ma Watch-List
+            </a>
+          </li>
+          <li>
+            {/*biome-ignore lint/a11y/useValidAnchor : lien de navigation a pour une ancre*/}
+            <a href="" className="profile-menu-titles" id="mes-parametres">
+              Paramètres
+            </a>
+          </li>
+          <li>
+            <button
+              type="button"
+              className="profile-menu-titles"
+              id="deconnexion"
+            >
+              Déconnexion
+            </button>
+          </li>
+        </ul>
+      </aside>
       <section className="profile-layout">
-        <h2 className="profile-account-title profile-list-title">Mon compte</h2>
+        <h2 className="profile-account-title profile-list-title" id="compte">
+          Mon compte
+        </h2>
         <div className="profile-account-wrapper">
           <div className="profile-my-account">
             <h3>Pseudo</h3>
@@ -93,7 +149,9 @@ function Profile() {
         </div>
 
         <div className="profile-list-wrapper">
-          <h2 className="profile-list-title">Mes favoris</h2>
+          <h2 className="profile-list-title" id="favoris">
+            Mes favoris
+          </h2>
           <div className="profile-list">
             <CarousselLarge
               items={favorites}
@@ -106,7 +164,9 @@ function Profile() {
         </div>
 
         <div className="profile-list-wrapper">
-          <h2 className="profile-list-title">Ma Watch-List</h2>
+          <h2 className="profile-list-title" id="watch-list">
+            Ma Watch-List
+          </h2>
           <div className="profile-list">
             <CarousselLarge
               items={toWatch}
@@ -118,7 +178,7 @@ function Profile() {
           </div>
         </div>
       </section>
-    </>
+    </section>
   );
 }
 
